252f81f1de3bbc261f1c42baa55fdb84f7bc7f44
Daniel Samuel <dansam5000@gmail.com>
2021-07-14 03:35:14 +0100

    Add validation to model

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,2 +1,4 @@
 class Opinion < ApplicationRecord
+
+  validates :text, presence: true, length: { maximum: 280 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { maximum: 10 }
+  validates :fullname, presence: true, length: { maximum: 60 }
+  before_save { username.downcase! }
 end
