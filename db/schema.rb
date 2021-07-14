638e45edee507e80ceec9034e0c1034ccfd06dfd
Daniel Samuel <dansam5000@gmail.com>
2021-07-14 21:12:48 +0100

    Add Opinion count column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_195407) do
+ActiveRecord::Schema.define(version: 2021_07_14_201005) do
 
   create_table "followings", force: :cascade do |t|
     t.integer "follower_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_07_14_195407) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "count_followers", default: 0
+    t.integer "opinion_count", default: 0
   end
 
 end
