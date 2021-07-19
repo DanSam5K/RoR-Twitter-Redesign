c87868a74247a93da646493d73c159dab33b9fca
Daniel Samuel <dansam5000@gmail.com>
2021-07-19 07:02:24 +0100

    Fix long text issues

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,6 +1,8 @@
 class Opinion < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  validates :text, presence: true, length: { maximum: 280 }
+  validates :text, presence: true, length: { maximum: 280,
+                                                too_long: '280 characters in comment is the maximum allowed.' }
+                                                
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
   scope :newsfeed_opinions, -> { includes(:author).ordered_by_most_recent }
 end
