7e5e50fe1d7715adecdb464570bafc2930d42848
Daniel Samuel <dansam5000@gmail.com>
2021-07-19 23:10:40 +0100

    Create and migrate like features model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_153202) do
+ActiveRecord::Schema.define(version: 2021_07_19_220221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2021_07_19_153202) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "opinion_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["opinion_id"], name: "index_likes_on_opinion_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "opinions", force: :cascade do |t|
     t.integer "author_id"
     t.string "text"
@@ -71,4 +80,6 @@ ActiveRecord::Schema.define(version: 2021_07_19_153202) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "likes", "opinions"
+  add_foreign_key "likes", "users"
 end
