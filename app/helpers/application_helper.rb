a2a91568ec0724666df1d4ed3cd898bfb8ebb438
Daniel Samuel <dansam5000@gmail.com>
2021-07-19 20:20:28 +0100

    Add helpers function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def show_cover_image(user)
+    if user.cover_image.attached?
+        image_tag(user.cover_image, alt: user.username, class: 'pr-2')
+    else
+        image_tag('https://source.unsplash.com/random/800x250', alt: user.username, class: 'pr-2')
+    end
+  end
+
+  def show_photo(user, extra_class = '')
+    if user.photo.attached?
+      image_tag(user.photo, alt: user.username, class: "pr-2 rounded profile-img #{extra_class}")
+    else
+      image_tag('https://source.unsplash.com/random/60x60',
+                alt: user.username, class: 'pr-2 rounded profile-img')
+    end
+  end
 end
