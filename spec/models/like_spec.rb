98abbbb471672b70672edf72f300ec44b2d6c02f
Daniel Samuel <dansam5000@gmail.com>
2021-07-20 15:29:31 +0100

    Add more test for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -6,5 +6,25 @@ RSpec.describe Like, type: :model do
       user = User.reflect_on_association(:likes)
       expect(user.macro).to eq(:has_many)
     end
+
+    it 'belongs to a user' do
+      likes = Like.reflect_on_association(:user)
+      expect(likes.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a user' do
+      likes = Like.reflect_on_association(:opinion)
+      expect(likes.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a opinion -ve' do
+      likes = Like.reflect_on_association(:opinion)
+      expect(likes.macro).not_to eq(:has_many)
+    end
+
+    it 'belongs to a user -ve ' do
+      likes = Like.reflect_on_association(:user)
+      expect(likes.macro).not_to eq(:has_many)
+    end
   end
 end
